430e2cf8e1668f34629ec1482650da24a0a00f2c
mateuswetah <mateus.m.luna@gmail.com>
2022-04-13 22:00:01 -0300

    Version 0.0.1

--- functions.php
+++ functions.php
@@ -1,8 +1,14 @@
 <?php
+
 if (! defined('WP_DEBUG')) {
 	die( 'Direct access forbidden.' );
 }
+
+/** Child Theme version */
+const GABINETE_VERSION = '0.0.1';
+
+/* Enqueues necessary JS and CSS files */
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'gainete-style', './style.css' );
+	wp_enqueue_style( 'gabinete-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), GABINETE_VERSION );
 });
